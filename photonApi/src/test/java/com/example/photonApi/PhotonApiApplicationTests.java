df5d04372da783586b6ca72dcc3d473d9a15bd93
limonchik37 <alim.bek@abra.eu>
2024-09-01 21:40:55 +0200

    Added PhotonApi

---
+++ photonApi/src/test/java/com/example/photonApi/PhotonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.photonApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
